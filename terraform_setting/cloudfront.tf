9a26bfccaf5a4a5aad7a0c9a546c5d6958803528
Byeonghyeon <tw8542@naver.com>
2024-07-23 09:40:55 +0900

    media setting v1

--- terraform_setting/cloudfront.tf
+++ terraform_setting/cloudfront.tf
@@ -1,8 +1,14 @@
 resource "aws_cloudfront_distribution" "nono_distribution" {
   enabled             = true
   is_ipv6_enabled     = true
-  price_class         = "PriceClass_All"
   http_version        = "http2"
+  price_class         = "PriceClass_All"
+
+  depends_on = [
+    aws_lambda_function.create_mediaconvert_job,
+    aws_s3_bucket.nonooutput,
+    aws_s3_bucket.nonooutput_ca_central_1
+  ]
 
   origin {
     domain_name = "nonooutput.s3.ap-northeast-2.amazonaws.com"
@@ -50,6 +56,8 @@ resource "aws_cloudfront_distribution" "nono_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+  web_acl_id = null # WAF 비활성화
 }
 
 resource "aws_cloudfront_origin_access_identity" "nono_oai" {
@@ -57,28 +65,9 @@ resource "aws_cloudfront_origin_access_identity" "nono_oai" {
 }
 
 # S3 버킷 정책 업데이트 (nonooutput 버킷)
-resource "aws_s3_bucket_policy" "nonooutput_policy" {
-  bucket = "nonooutput"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontOAI"
-        Effect    = "Allow"
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.nono_oai.iam_arn
-        }
-        Action   = "s3:GetObject"
-        Resource = "arn:aws:s3:::nonooutput/*"
-      }
-    ]
-  })
-}
-
-# S3 버킷 정책 업데이트 (nonooutput-ca-central-1 버킷)
 resource "aws_s3_bucket_policy" "nonooutput_ca_central_1_policy" {
-  bucket = "nonooutput-ca-central-1"
+  provider = aws.ca-central-1  # ca-central-1 리전의 프로바이더 사용
+  bucket = aws_s3_bucket.nonooutput_ca_central_1.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -90,7 +79,7 @@ resource "aws_s3_bucket_policy" "nonooutput_ca_central_1_policy" {
           AWS = aws_cloudfront_origin_access_identity.nono_oai.iam_arn
         }
         Action   = "s3:GetObject"
-        Resource = "arn:aws:s3:::nonooutput-ca-central-1/*"
+        Resource = "${aws_s3_bucket.nonooutput_ca_central_1.arn}/*"
       }
     ]
   })
--- terraform_setting/iam.tf
+++ terraform_setting/iam.tf
@@ -1,10 +1,12 @@
-provider "aws" {
-  region = "ap-northeast-2"  # 또는 사용하려는 리전
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
 }
 
-# IAM 역할 생성
+# MediaConvert Role
 resource "aws_iam_role" "mediaconvert_role" {
-  name = "mediaconvert_role"
+  name = "mediaconvert_role_${random_string.suffix.result}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -18,11 +20,18 @@ resource "aws_iam_role" "mediaconvert_role" {
       }
     ]
   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-# API Gateway Invoke 정책
+locals {
+  mediaconvert_role_id = aws_iam_role.mediaconvert_role.id
+}
+
 resource "aws_iam_role_policy" "api_gateway_invoke_policy" {
-  name = "api_gateway_invoke_policy"
+  name = "api_gateway_invoke_policy_${random_string.suffix.result}"
   role = aws_iam_role.mediaconvert_role.id
 
   policy = jsonencode({
@@ -38,11 +47,14 @@ resource "aws_iam_role_policy" "api_gateway_invoke_policy" {
       }
     ]
   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-# S3 Full Access 정책
-resource "aws_iam_role_policy" "s3_full_access_policy" {
-  name = "s3_full_access_policy"
+resource "aws_iam_role_policy" "s3_access_policy" {
+  name = "s3_access_policy_${random_string.suffix.result}"
   role = aws_iam_role.mediaconvert_role.id
 
   policy = jsonencode({
@@ -51,11 +63,125 @@ resource "aws_iam_role_policy" "s3_full_access_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:*",
-          "s3-object-lambda:*"
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:GetBucketLocation"
         ]
-        Resource = "*"
+        Resource = [
+          "arn:aws:s3:::nonoinput",
+          "arn:aws:s3:::nonoinput/*",
+          "arn:aws:s3:::nonooutput",
+          "arn:aws:s3:::nonooutput/*",
+          "arn:aws:s3:::nonooutput-us-east-1",
+          "arn:aws:s3:::nonooutput-us-east-1/*",
+          "arn:aws:s3:::nonooutput-ca-central-1",
+          "arn:aws:s3:::nonooutput-ca-central-1/*"
+        ]
       }
     ]
   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+# Lambda VOD Execution Role
+resource "aws_iam_role" "lambda_vod_execution" {
+  name = "lambda_vod_execution_${random_string.suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+locals {
+  lambda_vod_execution_id = aws_iam_role.lambda_vod_execution.id
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_vod_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_vod_execution_policy" {
+  name = "lambda_vod_execution_policy_${random_string.suffix.result}"
+  role = aws_iam_role.lambda_vod_execution.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::nonoinput",
+          "arn:aws:s3:::nonoinput/*",
+          "arn:aws:s3:::nonooutput",
+          "arn:aws:s3:::nonooutput/*",
+          "arn:aws:s3:::nonooutput-us-east-1",
+          "arn:aws:s3:::nonooutput-us-east-1/*",
+          "arn:aws:s3:::nonooutput-ca-central-1",
+          "arn:aws:s3:::nonooutput-ca-central-1/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "mediaconvert:CreateJob",
+          "mediaconvert:GetJob",
+          "mediaconvert:ListJobs",
+          "mediaconvert:CancelJob",
+          "mediaconvert:DescribeEndpoints"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = "iam:PassRole"
+        Resource = aws_iam_role.mediaconvert_role.arn
+      },
+      {
+        Effect   = "Allow"
+        Action   = "lambda:InvokeFunction"
+        Resource = "arn:aws:lambda:ap-northeast-2:975049989858:function:create_mediaconvert_job*"
+      }
+    ]
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
