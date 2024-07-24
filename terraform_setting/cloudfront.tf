89ebd2c02da5d363813b05710c21f55750453cb3
Byeonghyeon <tw8542@naver.com>
2024-07-24 15:31:22 +0900

    클라우드 프런트 동작 추가

--- terraform_setting/cloudfront.tf
+++ terraform_setting/cloudfront.tf
@@ -1,38 +1,19 @@
 resource "aws_cloudfront_distribution" "nono_distribution" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  http_version        = "http2"
-  price_class         = "PriceClass_All"
-
+  enabled         = true
+  is_ipv6_enabled = true
+  http_version    = "http2"
+  price_class     = "PriceClass_All"
   depends_on = [
     aws_lambda_function.create_mediaconvert_job,
     aws_s3_bucket.nonooutput,
-    aws_s3_bucket.nonooutput_ca_central_1
+    aws_s3_bucket.nonooutput_ca_central_1,
+    aws_s3_bucket.nonooutput_us_east_1,
+    aws_s3_bucket.nonooutput_ap_northeast_2
   ]
 
   origin {
-    domain_name = "nonooutput.s3.ap-northeast-2.amazonaws.com"
-    origin_id   = "nonooutput-origin"
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.nono_oai.cloudfront_access_identity_path
-    }
-  }
-
-  origin {
-    domain_name = "nonooutput-ca-central-1.s3.ca-central-1.amazonaws.com"
-    origin_id   = "nonooutput-ca-central-1-origin"
-
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.nono_oai.cloudfront_access_identity_path
-    }
-  }
-
-#07.24 추가 내용 [원본 도메인 서울리전, 미국리전 추가]
-  origin {
     domain_name = "nonooutput-us-east-1.s3.us-east-1.amazonaws.com"
     origin_id   = "nonooutput-us-east-1-origin"
-
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.nono_oai.cloudfront_access_identity_path
     }
@@ -41,7 +22,14 @@ resource "aws_cloudfront_distribution" "nono_distribution" {
   origin {
     domain_name = "nonooutput-ap-northeast-2.s3.ap-northeast-2.amazonaws.com"
     origin_id   = "nonooutput-ap-northeast-2-origin"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.nono_oai.cloudfront_access_identity_path
+    }
+  }
 
+  origin {
+    domain_name = "nonooutput-ca-central-1.s3.ca-central-1.amazonaws.com"
+    origin_id   = "nonooutput-ca-central-1-origin"
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.nono_oai.cloudfront_access_identity_path
     }
@@ -50,16 +38,68 @@ resource "aws_cloudfront_distribution" "nono_distribution" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "nonooutput-origin"
-
+    target_origin_id = "nonooutput-us-east-1-origin"
     forwarded_values {
       query_string = false
       cookies {
         forward = "none"
       }
     }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
 
-    viewer_protocol_policy = "allow-all"
+  ordered_cache_behavior {
+    path_pattern     = "/us/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "nonooutput-us-east-1-origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/kr/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "nonooutput-ap-northeast-2-origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/ca/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "nonooutput-ca-central-1-origin"
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
@@ -80,5 +120,5 @@ resource "aws_cloudfront_distribution" "nono_distribution" {
 }
 
 resource "aws_cloudfront_origin_access_identity" "nono_oai" {
-  comment = "OAI for nonooutput S3 bucket"
+  comment = "OAI for nonooutput S3 buckets"
 }
