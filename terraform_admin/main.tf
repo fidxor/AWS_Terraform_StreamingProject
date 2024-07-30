29e63cef79c7a227809740b983eb6e772ce00154
tmdalsdl <lsm123452002@gmail.com>
2024-07-30 12:04:23 +0900

    main.tf 수정

--- terraform_admin/main.tf
+++ terraform_admin/main.tf
@@ -1,23 +1,75 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = "ap-northeast-2"
 }
 
-# Data source to fetch existing security group
-data "aws_security_group" "existing" {
-  name = var.security_group_name
+# Create a new security group
+resource "aws_security_group" "admin_server_sg" {
+  name        = "admin-server-sg"
+  description = "Security group for Admin server"
+
+  # Inbound rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule (allow all outbound traffic)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Create an EC2 instance
 resource "aws_instance" "admin_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami           = "ami-062cf18d655c0b1e8"
+  instance_type = "t2.micro"
+  key_name      = "AdminPage-test"
 
   tags = {
-    Name = var.instance_name
+    Name = "Admin-server"
   }
 
-  # Use the existing security group
-  vpc_security_group_ids = [data.aws_security_group.existing.id]
+  # Use the newly created security group
+  vpc_security_group_ids = [aws_security_group.admin_server_sg.id]
 }
